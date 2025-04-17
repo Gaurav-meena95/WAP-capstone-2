c6b161ecc3eb70405a1b3b7c11f0471901193402
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-04-17 21:55:00 +0530

    add csss file

--- Wap-Capstone-2/src/App.jsx
+++ Wap-Capstone-2/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [left, setLeft] = useState(100)
+  const homeref = useRef();
+  const aboutref = useRef();
+  const contactref = useRef();
 
   return (
     <>
-    <h1>lets Start</h1>
+ 
     </>
   )
 }
