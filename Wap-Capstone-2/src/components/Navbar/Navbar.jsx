64c4e17954cbe14c133a15c7963b4620150c7002
Gaurav Meena <meenajigaurav9589@gmail.com>
2025-04-23 23:28:23 +0530

    errror

--- Wap-Capstone-2/src/components/Navbar/Navbar.jsx
+++ Wap-Capstone-2/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import homeMain from "../assets/homeMain.jpeg"; 
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-      <div className="nav_logo">
-        <h1>Auction</h1>
+    <>
+      <div className="navbar">
+        <div className="nav_logo">
+          <h1>Auction</h1>
+        </div>
+        <div className="nav_links">
+          <a href="#">Home</a>
+          <a href="#">Auctions</a>
+          <a href="#">About</a>
+          <a href="#">Contact us</a>
+          <button className="nav_btn">login/register</button>
+        </div>
       </div>
-      <div className="nav_links">
-        <a href="#">Home</a>
-        <a href="#">Auctions</a>
-        <a href="#">About</a>
-        <a href="#">Contact us</a>
+      <div className="main_page">
+        <img src={homeMain} alt="" />
       </div>
-      <div className="nav_action">
-        <button className='nav_btn'>login/register</button>
-      </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
